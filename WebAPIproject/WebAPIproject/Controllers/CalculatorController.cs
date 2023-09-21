5029344afa8482402c8ced5fbe9909ebebcb479b
Srivishnu Balaji <srivishnu.balaji@gavstech.com>
2023-09-21 12:04:52 +0530

    FirstWebAPI

---
+++ WebAPIproject/WebAPIproject/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        //api/[Calculator]/Add?x=10&y=2
+       [HttpGet("/Add")]
+        
+        public int Add(int x, int y)
+        {
+            return x+y;
+        }
+        //api/[Calculator]/Subtract?x=11&y=2
+        [HttpGet("/Subtract")]
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        //api/[Calculator]/Multiply?x=12&y=2
+        [HttpGet("/Multiply")]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        //api/[Calculator]/Divide?x=14&y=2
+        [HttpGet("/Divide")]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+
+    }
+}
---
+++ WebAPIproject/WebAPIproject/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("Demo/Add")]
+        public int Add(int x, int y)
+        {
+            return x + y + 50;
+        }
+        [HttpGet("Demo/Subtract")]
+        public int Subtract(int x, int y)
+        {
+            return x - y - 10;
+        }
+        //api/[Calculator]/Multiply?x=12&y=2
+        [HttpGet("Demo/Multiply")]
+        public int Multiply(int x, int y)
+        {
+            return x * y * x;
+        }
+        //api/[Calculator]/Divide?x=14&y=2
+        [HttpGet("Demo/Divide")]
+        public int Divide(int x, int y)
+        {
+            return (x / y) / y;
+        }
+    }
+}
