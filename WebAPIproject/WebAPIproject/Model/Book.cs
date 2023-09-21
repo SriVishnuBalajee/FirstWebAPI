7c245c09d971894d1a96069c75967d274a380fab
srivishnu.balaji <srivishnu.balaji@200411LTP2869>
2023-09-21 14:01:58 +0530

    include book and book controller

---
+++ WebAPIproject/WebAPIproject/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WebAPIproject.Model
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Cost { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
